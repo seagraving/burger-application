621ea808b33730373aeb43eb14d6e7dc6832be68
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-10-04 11:35:40 -0500

    added schema and orm connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bulgogi Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Hamburger', false);
